2c978f7fd88ea94f4ab2159ab4df44f6ddca4bf4
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-01-07 22:23:32 -0500

    un modal

--- lib/core/widgets/card/card_widget.dart
+++ lib/core/widgets/card/card_widget.dart
@@ -20,6 +20,55 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void showModal() {
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.vertical(top: Radius.circular(20)),
+        ),
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsetsGeometry.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+              left: 16,
+              right: 16,
+              top: 20,
+            ),
+            child: Column(
+              children: [
+                Image.network(
+                  urlToImage,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Icon(
+                      Icons.image_not_supported,
+                      size: 80,
+                      color: Colors.grey,
+                    );
+                  },
+                ),
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'By ${author.isEmpty ? 'Anonimo' : author}',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 12),
+                ),
+                Text(description),
+                Text(
+                  '${DateTime.parse(publishedAt).day}/${DateTime.parse(publishedAt).month}/${DateTime.parse(publishedAt).year}',
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+
     return Container(
       padding: EdgeInsets.all(12),
       margin: EdgeInsets.only(bottom: 15),
@@ -27,6 +76,7 @@ class CardWidget extends StatelessWidget {
         border: Border.all(color: Colors.grey, width: 1),
       ),
       child: InkWell(
+        onTap: showModal,
         child: Column(
           children: [
             Image.network(
